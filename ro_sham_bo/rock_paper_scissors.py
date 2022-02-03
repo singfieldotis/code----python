c0e6bb0626c3e78bba177facd234879d596bb9ac
Otis Singfield <singfieldotis@gmail.com>
2022-02-03 02:38:51 -0500

    Added RPS, number generators, and a hangman game

---
+++ ro_sham_bo/rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random
+
+
+def play():
+    user = input(
+        "What's your choice? 'r' for rock, 'p' for paper, 's' for scissors: \n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return f'The computer chose {computer}. It\'s a tie'
+
+    if is_win(user, computer):
+        return f'The computer chose {computer}. You won!'
+
+    return f'The computer chose {computer}. You lost!'
+
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
+
+
+print(play())
